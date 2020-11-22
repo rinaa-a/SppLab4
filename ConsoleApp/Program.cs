f80d1ce1349106f5834409d2ed6b019be849abdc
arinab123@gmail.com <arinab123@gmail.com>
2020-11-22 17:12:08 +0300

    Add console app project

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks.Dataflow;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private const int maxFilesToLoad = 5;
+        private const int maxTasks = 5;
+        private const int maxFilesToWrite = 5;
+
+        private const string destPath = "D:\\5sem\\spp\\lab4\\";
+        private static readonly string[] filesPathes =
         {
-            Console.WriteLine("Hello World!");
+            ""
+        };
+        static void Main()
+        {
+            var loadSourceFiles = new TransformBlock<string, string>(async path =>
+            {
+                Console.WriteLine("Loading {0} ...", path);
+
+                using StreamReader reader = File.OpenText(path);
+                return await reader.ReadToEndAsync();
+            },
+            new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxFilesToLoad
+            });
+
+            var generateTestClasses = new TransformManyBlock<string, T>(async source =>
+            {
+                Console.WriteLine("Generating test classes ...");
+                // TODO generate test classes
+                
+            },
+            new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxTasks
+            });
+
+
+            var writeToFile = new ActionBlock<T>(async testClass =>
+            {
+                Console.WriteLine("Writing {0} to file...", testClass.Name);
+
+                using StreamWriter writer = File.CreateText(destPath);
+                await writer.WriteAsync(testClass.value);
+            },
+            new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxFilesToWrite
+            });
+
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+            loadSourceFiles.LinkTo(generateTestClasses, linkOptions);
+            generateTestClasses.LinkTo(writeToFile, linkOptions);
+
+            foreach (var item in filesPathes)
+            {
+                loadSourceFiles.Post(item);
+            }
+            
+            // Mark the head of the pipeline as complete.
+            loadSourceFiles.Complete();
+
+            // Wait for the last block in the pipeline to process all messages.
+            writeToFile.Completion.Wait();
         }
     }
 }
